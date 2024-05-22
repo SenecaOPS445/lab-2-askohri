3b28b9b7b67bc58ce95f22c90f8c399da91feee4
askohri <askohri@myseneca.ca>
2024-05-22 16:56:43 -0400

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Abhyjit Singh Kohri
+# Author ID: 142112226
+# Date Created: 2024/05/22
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
+
