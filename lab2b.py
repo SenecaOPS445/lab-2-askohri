37c96c7f05ac21773a3d9e9d57f004b77697eaed
askohri <askohri@myseneca.ca>
2024-05-22 16:53:12 -0400

    lab2b.py

---
+++ lab2b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Prompting user for name and age
+name = input("Name: ")
+age = input("Age: ")
+
+# Printing the output
+print("Hi " + name + ", you are " + age + " years old.")
---
+++ lab2c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Assigning command line arguments to variables
+name = sys.argv[1]
+age = int(sys.argv[2])
+
+# Printing the output
+print("Hi " + name + ", you are " + str(age) + " years old.")
